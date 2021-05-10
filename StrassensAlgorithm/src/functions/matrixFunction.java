d6164f66a2eff4a02bbf6f34d517be6bb72404d2
Josh Wood <jw4302@cs.ship.edu>
2021-05-09 23:30:48 -0400

    restructuing, Abstracting Function

---
+++ StrassensAlgorithm/src/functions/matrixFunction.java
@@ -0,0 +1,7 @@
+package functions;
+
+public interface matrixFunction {
+
+	public int [][] execute(int [][] matrixOne, int [][] matrixTwo);
+	
+}
---
+++ StrassensAlgorithm/src/runner.java
@@ -0,0 +1,14 @@
+import functions.strassensFunction;
+import functions.bruteForceFunction;
+import functions.matrixFunction;
+
+public class runner {
+
+
+	
+	public static void main(String args) {
+		Sca
+		matrixFunction function = bruteForceFunction.getInstance();
+
+	}
+}
